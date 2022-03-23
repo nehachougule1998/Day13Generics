98ae797f593d7403456e6f0c7467f62a974cca29
nehachougule1998 <nchougule734@gmail.com>
2022-03-23 15:24:00 +0530

    UC3 added

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,11 +1,12 @@
 package com.bridgelabz;
 
 public class Main {
-
 	public static void main(String[] args) {
 		
 		Testmaximum test1 = new Testmaximum();
-		Integer[] inputArray = {2,3,4};
-		test1.maxElement(inputArray);
+		String[] inputArray = {"mango","strawberry","grapes"};
+	    test1.maxElement(inputArray);
 	}
+	
+
 }
