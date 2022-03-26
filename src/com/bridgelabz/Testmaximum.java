276dc5b8ddec59bfc3f318d52d87b9ee8491e5bb
nehachougule1998 <nchougule734@gmail.com>
2022-03-26 20:30:26 +0530

    Refactor1 added

--- src/com/bridgelabz/Testmaximum.java
+++ src/com/bridgelabz/Testmaximum.java
@@ -1,22 +1,18 @@
 package com.bridgelabz;
 
 public class Testmaximum<T extends Comparable<T>> {
-private T[] intArray;
 	
-	Testmaximum(T[] intArray){
-		this.intArray = intArray;
-	}	
-
-	public void  maxElement() {
-		for(int i=0; i<intArray.length-1; i++) {
-			if(intArray[i].compareTo(intArray[i+1]) > 0) {
-				intArray[i+1] = intArray[i];
-			}
+		private T[] intArray;
+		Testmaximum(T[] intArray) {
+			this.intArray = intArray;
 		}
-		printMax();
+		
+		public void maxElement() {
+			for(int i=0; i<intArray.length-1; i++) {
+				if(intArray[i].compareTo(intArray[i+1]) == 1) {
+					intArray[i+1] = intArray[i];
+				}
+			}
+			System.out.println(intArray[intArray.length-1]);
+		}
 	}
-	
-	private void printMax() {
-		System.out.println(intArray[intArray.length - 1]);
-	}
-}
